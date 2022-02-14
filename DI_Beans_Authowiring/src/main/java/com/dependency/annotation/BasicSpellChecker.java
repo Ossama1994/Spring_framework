376ec38c7b8664ea10cb3e2f76cd2f15fb76b185
ossama <ossamamahmoudy@gmail.com>
2022-02-14 15:20:41 +0100

    Authowired/Name/Type

---
+++ DI_Beans_Authowiring/src/main/java/com/dependency/annotation/BasicSpellChecker.java
@@ -0,0 +1,21 @@
+package com.dependency.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BasicSpellChecker implements SpellChecker{
+    @Override
+    public void checkSpelling(String emailMessage) {
+        if (emailMessage != null)
+        {
+            System.out.println("Checking spelling using BasicSpellChecker...");
+            System.out.println("Spell Checking Completed!!");
+        } else
+        {
+            throw new RuntimeException("An exception occurred " +
+                    "while checking Spelling");
+
+        }
+    }
+}
---
+++ DI_Beans_Authowiring/src/main/java/com/dependency/annotation/SpellChecker.java
@@ -0,0 +1,5 @@
+package com.dependency.annotation;
+
+public interface SpellChecker {
+    void checkSpelling(String emailMessage);
+}
